c2cffbb1defc5a6fcf66672dd8991534dd0084a1
vidhya <vidhya@santhos.com>
2022-08-22 12:44:25 +0530

    added array in js file 7th commit

--- variables.js
+++ variables.js
@@ -14,7 +14,16 @@ let y = 6;
 let z = x + y;
 document.getElementById("demo").innerHTML =
 "The value of z is: " + z;
+
+<script>
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits;
+fruits.push("Kiwi");
+document.getElementById("demo2").innerHTML = fruits;
+
+</script>
 </script>
 
 </body>
+
 </html>
