5b50314ea60671563576edcb80ba17c6c6f94261
vidhya <vidhya@santhos.com>
2022-08-23 18:34:11 +0530

    commit 123 in test.php file in deve branch

--- test.php
+++ test.php
@@ -3,4 +3,8 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
+
+echo "this is the time formate iun php"<br>";
+
 ?>
+
